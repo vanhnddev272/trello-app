9c8dd9dcca7716bc0f6462b546e76bf8b54093cb
Viet Anh <vanhhavesomuchcute@gmail.com>
2023-11-24 02:56:49 +0700

    Add app bar.

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box'
 function BoardContent() {
   return (
     <Box sx={{
-      backgroundColor: '#00e676',
+      backgroundColor: 'secondary.A400',
       width: '100%',
       height: (theme) => `calc(100vh - ${theme.trello.navBarHeight} - ${theme.trello.boardBarHeight})`,
       display: 'flex',
