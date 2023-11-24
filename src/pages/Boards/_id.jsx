8a0ed28b28e4d217a28c7d7428f1118e18dbb114
Viet Anh <vanhhavesomuchcute@gmail.com>
2023-11-25 02:28:28 +0700

    Add board bar.

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,22 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar'
 import BoardBar from './BoardBar'
 import BoardContent from './BoardContent'
+import Box from '@mui/material/Box'
+import bg from '~/assets/bg1.png'
 
 function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar />
-      <BoardContent />
+      <Box
+        sx={{
+          backgroundImage: `url(${bg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}>
+        <BoardBar />
+        <BoardContent />
+      </Box>
     </Container>
   )
 }
